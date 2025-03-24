e9ffb769580092169d69b9a613586875e8b557ee
Sean McClure <sean.mcclure17@gmail.com>
2025-03-23 22:25:09 -0400

    use sentry lambda integration

--- sheetsapi/sentry_helpers.py
+++ sheetsapi/sentry_helpers.py
@@ -1,6 +1,7 @@
 import logging
 import sentry_sdk
 from sheetsapi import config
+from sentry_sdk.integrations.aws_lambda import AwsLambdaIntegration
 
 
 logger = logging.getLogger(__name__)
@@ -11,6 +12,7 @@ def init():
     logger.info("Initializing Sentry SDK")
     sentry_sdk.init(
         config.Config.Constants.SENTRY_DSN,
+        integrations=[AwsLambdaIntegration()],
         traces_sample_rate=1.0,
         environment=config.Config.Constants.ENVIRONMENT,
     )
