bf602eb998ff7eee5508a9034b4fc8c3fc352956
Sean McClure <sean.mcclure17@gmail.com>
2025-09-27 10:57:41 -0400

    forgot to update republish

--- routers/docs.py
+++ routers/docs.py
@@ -211,6 +211,9 @@ async def update_content(data: PublishDocApiRequest, user: CurrentUser):
     api = doc_repo.get_api_metadata(data.owner_id, data.api_name)
     google_docs = google_docs_client.GoogleDocs.from_token_info(api.refresh_token_info)
     google_doc_payload = google_docs.get_document(api.google_doc_id)
+    parser = GoogleDocsParser(
+        docs_json=json.loads(api.google_doc_payload), image_handler=image_handler
+    )
     published_at = datetime.now().isoformat()
 
     try:
@@ -219,6 +222,7 @@ async def update_content(data: PublishDocApiRequest, user: CurrentUser):
             api_name=data.api_name,
             fields={
                 "google_doc_payload": json.dumps(google_doc_payload),
+                "google_doc_ast": json.dumps(node_to_dict(parser.parse())),
                 "title": google_doc_payload["title"],
                 "published_at": published_at,
             },
